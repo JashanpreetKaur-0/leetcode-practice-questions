de70c0026a0b719f1e3d8950905100288360759d
JashanpreetKaur-0 <joshanjashan854@gmail.com>
2026-09-22 11:04:20 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.15%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if(n==0){
+            return 0;
+        }
+        int[] nums=new int[n+1];
+        nums[0]=0;
+        nums[1]=1;
+        int max=1;
+        for(int i=0;i<=n;i++){
+            if(2*i<=n){
+                nums[2*i]=nums[i];
+            }
+            if(2*i+1<=n){
+                nums[2*i+1]=nums[i]+nums[i+1];
+            }
+        }
+        for(int i=0;i<=n;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+        return max;
+    }
+}
