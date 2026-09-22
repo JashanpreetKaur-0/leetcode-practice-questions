b2786201cc35ae9cf24c4232ecad58274a386771
JashanpreetKaur-0 <joshanjashan854@gmail.com>
2026-09-22 10:59:02 +0530

    Time: 15 ms (18.85%), Space: 46.2 MB (31.32%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int start=0;
+        int end=s.length()-1;
+        //boolean isplaindrom=true;
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end)){
+                return false;
+               
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
